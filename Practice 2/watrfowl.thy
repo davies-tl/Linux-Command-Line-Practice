68651923dfb296ec1874a54c8dc68cae0ab89fca
Tyler Davies <tldavies09@gmail.com>
2024-04-04 16:32:24 -0700

    uploading Practice 2

---
+++ Practice 2/watrfowl.thy
@@ -0,0 +1,9 @@
+4:   thy solitary way?
+6:   Might mark thy distant flight to do thee wrong, As,
+8:   thy figure floats along.
+14:   Teaches thy way along that pathless coast,
+17:All day thy wings have fanned,
+23:   And scream among thy fellows; reeds shall bend,
+24:   Soon, o'er thy sheltered nest.
+26:   Hath swallowed up thy form; yet on my heart
+30:   Guides through the boundless sky thy certain flight,
